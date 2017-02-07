d423c91271a5a52aa8d9b6affb85afe9ae71609a
Thibault PARPAITE <tparpaite@gmail.com>
2017-02-07 12:27:29 +0100

    Fin de l'exo 1     - Implementation des operations     - Trace du graphe     - Implementation algo log2

--- partie1/representation_decimale_reduite.py
+++ partie1/representation_decimale_reduite.py
@@ -1,4 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as mp
+
+
+MAX_ITE = 1000
+
 
 def rp(x, p):
     """ Calcule la representation decimale reduite d'un nombre
@@ -25,6 +30,63 @@ def rp(x, p):
     return round(x, p - val)
 
 
+def addition_rp(x, y, p):
+    return rp(rp(x, p) + rp(y, p), p)
+
+
+def multiplication_rp(x, y, p):
+    return rp(rp(x, p) * rp(y, p), p)
+
+
+def delta_add(x, y, p):
+    reel = x + y
+    machine = addition_rp(x, y ,p)
+
+    return abs(reel - machine) / abs(machine)
+
+
+def delta_mul(x, y, p):
+    reel = x * y
+    machine = multiplication_rp(x, y ,p)
+
+    return abs(reel - machine) / abs(machine)
+
+
+def afficher_delta(x, p):
+    abs_array = np.arange(0, 5, 0.01)
+    ord_delta_add = [delta_add(x, y, p) for y in abs_array]
+    ord_delta_mul = [delta_mul(x, y, p) for y in abs_array]
+    
+    mp.plot(abs_array, ord_delta_add, linewidth=1.0)
+    mp.plot(abs_array, ord_delta_mul, linewidth=1.0, color='r')
+
+    mp.xlabel("y")
+    mp.ylabel("delta")
+    mp.legend(["delta_add", "delta_mul"])
+    mp.title("Erreurs relatives de l'addition et la multiplication pour x = " + str(x))
+    mp.show()
+
+
+def log_2_v1(p):
+    res = 0
+    
+    for i in range(1, MAX_ITE):
+        tmp = (-1)**(i + 1) / float(i)
+        res = addition_rp(res, tmp, p)
+
+    return res
+
+
+def log_2_v2(p):
+    res = 0
+    
+    for i in range(MAX_ITE, 0, -1):
+        tmp = (-1)**(i + 1) / float(i)
+        res = addition_rp(res, tmp, p)
+
+    return res       
+        
+
 def main():
     x = 3.141592658
     y = 10507.1823
@@ -38,7 +100,15 @@ def main():
     print (str(z) + "\t" + str(rp(z, 4)) + "\t" + str(rp(z,6))).expandtabs(20)
     print (str(-x) + "\t" + str(rp(-x, 4)) + "\t" + str(rp(-x,6))).expandtabs(20)
 
+    # afficher_delta(10**(-4)/2, 5)
+    # vals pertinentes : 10**(-4), 0.0001857563
+    # 10**(-4)/2
+    # Precision machine limitee : 10**(-p + 1) / 2
+
 
 if __name__ == "__main__":
     main()
+
+
+# Comprehension de liste
     
